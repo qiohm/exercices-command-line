215c54af3a566a8a41cafbdc50bc0bc97b9163da
Gé Pé <aiohm@yahoo.fr>
2020-09-24 16:42:09 +0200

    add corr. palindromeChecker

--- src/palindromeChecker.js
+++ src/palindromeChecker.js
@@ -10,3 +10,15 @@ Alyra is not a palindrome */
 // correction de Ionnis
 
 // correction de Vladimir
+
+if (process.argv.length !== 3) {
+    console.log('usage: node palindromeChecker.js word')
+    process.exit(1)
+}
+
+let word = process.argv[2]
+if (word === word.split('').reverse().join('')) {
+    console.log(`${word} is a palindrome`)
+} else {
+    console.log(`${word} is not a palindrome`)
+}
